48175a4d327ab084e54e2eba736b627f9b7d1c4b
Prakhar Kant Tripathi <45271093+ppprakhar@users.noreply.github.com>
2024-11-12 18:52:31 +0530

    update

--- android/src/main/java/fr/ppprakhar/flutterappbadger/FlutterAppBadgerPlugin.java
+++ android/src/main/java/fr/ppprakhar/flutterappbadger/FlutterAppBadgerPlugin.java
@@ -1,52 +1,43 @@
 package fr.ppprakhar.flutterappbadger;
 
 import android.content.Context;
-
+import androidx.annotation.NonNull;
 import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
 import io.flutter.plugin.common.MethodChannel;
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
 import io.flutter.plugin.common.MethodChannel.Result;
-import io.flutter.plugin.common.MethodCall;
 import me.leolin.shortcutbadger.ShortcutBadger;
 
-/**
- * FlutterAppBadgerPlugin
- */
-public class FlutterAppBadgerPlugin implements MethodCallHandler, FlutterPlugin {
+/** FlutterAppBadgerPlugin */
+public class FlutterAppBadgerPlugin implements FlutterPlugin, MethodCallHandler {
+    private MethodChannel channel;
+    private Context context;
 
-  private Context applicationContext;
-  private MethodChannel channel;
-  private static final String CHANNEL_NAME = "ppprakhar/flutter_app_badger";
-
-  /**
-   * Plugin registration.
-   */
-
-  @Override
-  public void onAttachedToEngine(FlutterPluginBinding flutterPluginBinding) {
-    channel = new MethodChannel(flutterPluginBinding.getBinaryMessenger(), CHANNEL_NAME);
-    channel.setMethodCallHandler(this);
-    applicationContext = flutterPluginBinding.getApplicationContext();
-  }
-
-  @Override
-  public void onDetachedFromEngine(FlutterPluginBinding flutterPluginBinding) {
-    channel.setMethodCallHandler(null);
-    applicationContext = null;
-  }
-
-  @Override
-  public void onMethodCall(MethodCall call, Result result) {
-    if (call.method.equals("updateBadgeCount")) {
-      ShortcutBadger.applyCount(applicationContext, Integer.valueOf(call.argument("count").toString()));
-      result.success(null);
-    } else if (call.method.equals("removeBadge")) {
-      ShortcutBadger.removeCount(applicationContext);
-      result.success(null);
-    } else if (call.method.equals("isAppBadgeSupported")) {
-      result.success(ShortcutBadger.isBadgeCounterSupported(applicationContext));
-    } else {
-      result.notImplemented();
+    @Override
+    public void onAttachedToEngine(@NonNull FlutterPluginBinding binding) {
+        channel = new MethodChannel(binding.getBinaryMessenger(), "flutter_app_badger");
+        context = binding.getApplicationContext();
+        channel.setMethodCallHandler(this);
     }
-  }
+
+    @Override
+    public void onMethodCall(@NonNull MethodCall call, @NonNull Result result) {
+        if (call.method.equals("updateBadgeCount")) {
+            int count = call.argument("count");
+            if (count >= 0) {
+                ShortcutBadger.applyCount(context, count);
+            } else {
+                ShortcutBadger.removeCount(context);
+            }
+            result.success(null);
+        } else {
+            result.notImplemented();
+        }
+    }
+
+    @Override
+    public void onDetachedFromEngine(@NonNull FlutterPluginBinding binding) {
+        channel.setMethodCallHandler(null);
+    }
 }
